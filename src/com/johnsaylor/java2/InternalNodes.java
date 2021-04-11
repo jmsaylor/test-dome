3c7e06d89c29013f8e3aec4ba854cab2d8f59231
jmsaylor <johnmsaylor@gmail.com>
2021-04-11 15:57:40 -0400

    tree solution attempts

--- src/com/johnsaylor/java2/InternalNodes.java
+++ src/com/johnsaylor/java2/InternalNodes.java
@@ -13,6 +13,43 @@ public class InternalNodes {
         return result;
     }
 
+    public static int count2(int[] tree) {
+        int internalNodeCount = 0;
+        //find where start of first level is
+        // using 0 as a null value
+        int startIndex = tree[0] != 0 ? 1 : 2;
+        // move by two through length of array
+        for (int i = startIndex; (i + 2) < tree.length; i+=2) {
+            //if either pair is not null, indicates internal node so add to count
+            if (tree[i] > 0 || tree[i + 1] > 0) {
+                internalNodeCount++;
+            }
+        }
+
+        //return internal node count
+        return internalNodeCount;
+    }
+
+    public static int count3(int[] tree) {
+        //this solution assumes a complete and balanced binary tree
+        int internalNodeCount = 0;
+        int level = 0;
+
+        //find how many complete levels there are
+        while (tree.length > level * 2) {
+            level++;
+            //put number of complete levels through a formula that gives count of internal nodes, add this to count
+            internalNodeCount += (level / 2);
+        }
+        //get the cieling of the number of nodes in the incomplete level / 2 & add to count
+        int incompleteLevelNodeCount = tree.length - (level * 2);
+        if (incompleteLevelNodeCount % 2 == 1) incompleteLevelNodeCount+=1;
+        internalNodeCount = internalNodeCount + incompleteLevelNodeCount;
+        //return count
+        return internalNodeCount;
+    }
+
+
     public static void main(String[] args) {
         System.out.println(InternalNodes.count(new int[] { 1, 3, 1, -1, 3 }));
     }
